1c658273bfb41a7b486ae299b132438afcadf5d9
Moshe Plotkin <mplotkin@jewpaltz.com>
2020-10-05 15:27:16 -0400

    vue routing

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="section">
-    <div class="container">
+  <div class="">
       <Nav />
-      <router-view/>
-    </div>
-  </section>
+      <div class="container">
+          <router-view/>
+      </div>
+  </div>
 </template>
 
 <script>
