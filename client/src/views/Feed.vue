61c099646c66427bb0f27c2c9665ef5e1b9bf8cd
Moshe Plotkin <mplotkin@jewpaltz.com>
2020-10-08 14:46:31 -0400

    added basic design elements for feed

---
+++ client/src/views/Feed.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page">
+      <h2 class="title is-2">Feed Page</h2>
+
+        <div class="columns">
+            <div class="column is-two-thirds">
+                <Post /> <Post /> <Post /> <Post />
+            </div>
+            <div class="column is-one-third">
+                <Sidebar />
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from "@/components/Sidebar";
+import Post from "@/components/Post";
+
+export default {
+    components: {
+        Sidebar, Post
+    }
+}
+</script>
+
+<style>
+    .card {
+        margin-bottom: 30px ;
+    }
+</style>
