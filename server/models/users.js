3b4df9c8033babbe3fff6f2eb49619c9d715ee02
Moshe Plotkin <mplotkin@jewpaltz.com>
2020-10-19 15:21:19 -0400

    asynchronous programming

--- server/models/users.js
+++ server/models/users.js
@@ -4,13 +4,16 @@
 
 const data = [{ name: 'Moshe', age: 43}, { name: 'Biden', age: 78 }]
 
-function getAll(){
+async function getAll(){
+    //throw { status: 501, message: "This is a fake error" }
+    //await Promise.resolve()
+    console.log("Called Get All")
     return data;
 }
 
-function add(name, age){
+async function add(name, age){
     data.push({name, age});
 }
 
 
-module.exports = { getAll, add, search: q => data.filter(x=> x.name == q) }
+module.exports = { getAll, add, search: async q => data.filter(x=> x.name == q) }
